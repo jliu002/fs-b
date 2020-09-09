c6e10003eb0677db3ce9c2955f7fcb5dfd1bdd63
jliu002@ets.org <jliu002@ets.org>
2020-09-09 14:50:43 -0400

    first commit

---
+++ precache-manifest.466947b359ec5afe2e96c1e056118b52.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6265fd4861c14c82e0175a0a5bc028d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "920c4b7ebed68e1882c8",
+    "url": "/static/css/2.fa349ff9.chunk.css"
+  },
+  {
+    "revision": "f669b1184cc46652d513",
+    "url": "/static/css/main.37c1113f.chunk.css"
+  },
+  {
+    "revision": "920c4b7ebed68e1882c8",
+    "url": "/static/js/2.5738807e.chunk.js"
+  },
+  {
+    "revision": "5430312c5dd50ff1aa0dfaca387a4d59",
+    "url": "/static/js/2.5738807e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f669b1184cc46652d513",
+    "url": "/static/js/main.9ec4b11d.chunk.js"
+  },
+  {
+    "revision": "3bf81a68cead61a39df3",
+    "url": "/static/js/runtime-main.3e5d480a.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
